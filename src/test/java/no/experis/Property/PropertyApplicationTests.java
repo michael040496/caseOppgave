dc90a043ecef287b94329c2a86a433d3535c0645
Quoc Viet N. Tran <quocviettran95@gmail.com>
2019-04-08 10:43:49 +0200

    changed to spring

---
+++ src/test/java/no/experis/Property/PropertyApplicationTests.java
@@ -0,0 +1,16 @@
+package no.experis.Property;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PropertyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
