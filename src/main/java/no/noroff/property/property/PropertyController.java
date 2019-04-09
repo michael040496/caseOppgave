f6217f0f6a487010e0024ee08e50637612499197
Sanay <sanay1910@gmail.com>
2019-04-09 14:21:43 +0200

    Added search by id method

--- src/main/java/no/noroff/property/property/PropertyController.java
+++ src/main/java/no/noroff/property/property/PropertyController.java
@@ -1,6 +1,5 @@
 package no.noroff.property.property;
 
-import com.fasterxml.jackson.databind.annotation.JsonAppend;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
@@ -24,6 +23,30 @@ public class PropertyController {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
+
+    @GetMapping("/properties/{ID}")
+    public ResponseEntity<Property> getPropertyById(@PathVariable int id) {
+        try {
+            List<Property> propertyList = propertyRepository.findAll();
+            System.out.println("Trying to find character: " + id);
+
+            Property returnProperty = null;
+            for (Property p : propertyList) {
+                if (p.getProperty_id() == (id)) {
+                    System.out.println(" --- PROPERTY FOUND --- ");
+
+                    returnProperty = p;
+                }
+            }
+            if (returnProperty == null) {
+                System.out.println(" --- PROPERTY WAS NOT FOUND --- ");
+            }
+            return new ResponseEntity<>(returnProperty, HttpStatus.OK);
+        } catch(DataAccessException e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
 
 
 }
