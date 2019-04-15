f2b226fc3aab4b80fdd44f90766c42b60d6f302e
Billy Infante <infbil15@gmail.com>
2019-04-15 15:40:39 +0200

    Fixed authentication import

--- src/main/java/no/noroff/property/config/SecurityConfig.java
+++ src/main/java/no/noroff/property/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -26,7 +25,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/h2-console/**")
                 .antMatchers("/swagger-ui/index.html")
                 .antMatchers("/api/**")
-                .antMatchers("/**")
         ;
 
 
